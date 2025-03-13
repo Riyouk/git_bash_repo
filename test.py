42f317e498dc4a8c651b761d9fcdeb700c1c9bce
riyouk <gostoftheuchiha1@gmail.com>
2025-03-13 10:16:38 +0530

    adding functions to the file

---
+++ test.py
@@ -0,0 +1,13 @@
+def calculation (operation):
+    return operation
+
+def add (a,b):
+    print(f"the sum of these numbers is {a+b}")
+
+def sub(a,b):
+    print(f"the diffrence of these numbers is {a-b}")
+
+def multiply(a,b):
+    print(f"the product of these numbers is {a*b}")
+
+calculation(add(2,4))
